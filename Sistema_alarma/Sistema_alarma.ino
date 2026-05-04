056b192ca45e9deb38826324de61b5d240d9c4a6
MauroGc19 <mauricio.gonzales35417@ucaldas.edu.co>
2026-05-04 17:08:26 -0500

    CReacion de las clases del proyecto

---
+++ Sistema_alarma/Sistema_alarma.ino
@@ -0,0 +1,13 @@
+#include <LiquidCrystal.h>
+#include <Keypad.h>
+#include <SevSeg.h>
+#include <SPI.h>
+#include <MFRC522.h>
+
+#include "desarmado.h"
+#include "armado.h"
+#include "activado.h"
+#include "cronometro.h"
+
+void setup() {};
+void loop(){};
